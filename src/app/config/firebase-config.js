7c1bc3c4ad29a4ca2647a31599866d5141736289
Chris Sullivan <csullivan@reliaslearning.com>
2018-07-10 15:04:16 -0400

    took out the config api stuff

---
+++ src/app/config/firebase-config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyANUZczCqjaQT2t1NuMHhaaa0wWdEEtZJE',
+  authDomain: 'gnosiscounter.firebaseapp.com',
+  databaseURL: 'https://gnosiscounter.firebaseio.com',
+  projectId: 'gnosiscounter',
+  storageBucket: 'gnosiscounter.appspot.com',
+  messagingSenderId: '397480090874'
+};
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as firebase from 'firebase';
+import { config } from './app/config/firebase-config.js';
 
 
 import 'hammerjs';
@@ -11,14 +12,7 @@ import 'hammerjs';
 if (environment.production) {
   enableProdMode();
 }
-const config = {
-  apiKey: 'AIzaSyANUZczCqjaQT2t1NuMHhaaa0wWdEEtZJE',
-  authDomain: 'gnosiscounter.firebaseapp.com',
-  databaseURL: 'https://gnosiscounter.firebaseio.com',
-  projectId: 'gnosiscounter',
-  storageBucket: 'gnosiscounter.appspot.com',
-  messagingSenderId: '397480090874'
-};
+
 firebase.initializeApp(config);
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
